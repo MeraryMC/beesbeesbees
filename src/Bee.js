84be7f890d2d205fedb1f9d83a45b443c6765701
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-01-09 10:54:37 -0800

    implemented Bee class functionality

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
-var Bee = function() {
+var Bee = function(value) {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+Bee.prototype.job = 'keep on growing';
+
+
+
 
